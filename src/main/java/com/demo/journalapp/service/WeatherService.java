b07fedfdc6a7a0a0a4d368aebdcb8a518ba9ebbb
Anadi Mishra <62553697+anadi223@users.noreply.github.com>
2024-04-29 20:41:36 +0530

    Enhanced API call performance from 2.7sec to under 300milliseconds using Redis

--- src/main/java/com/demo/journalapp/service/WeatherService.java
+++ src/main/java/com/demo/journalapp/service/WeatherService.java
@@ -3,6 +3,7 @@ package com.demo.journalapp.service;
 import com.demo.journalapp.dtos.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -24,11 +25,21 @@ public class WeatherService {
     @Autowired
     public RestTemplate restTemplate;
 
+    @Autowired
+    private RedisService redisService;
 
     public WeatherResponse getWeather(String city) {
-        String finalURL = URL.replace("CITY", city).replace("API_KEY", apiKey);
-        ResponseEntity<WeatherResponse> responseEntity = restTemplate.exchange(finalURL, HttpMethod.GET, null, WeatherResponse.class);
-        return responseEntity.getBody();
-
+        WeatherResponse weatherResponse = redisService.get("weather_of_"+ city, WeatherResponse.class);
+        if(weatherResponse != null){
+            return weatherResponse;
+        }else{
+            String finalURL = URL.replace("CITY", city).replace("API_KEY", apiKey);
+            ResponseEntity<WeatherResponse> responseEntity = restTemplate.exchange(finalURL, HttpMethod.GET, null, WeatherResponse.class);
+            WeatherResponse body = responseEntity.getBody();
+            if(body != null){
+                redisService.set("weather_of_"+city, body,3000);
+            }
+            return responseEntity.getBody();
+        }
     }
 }
